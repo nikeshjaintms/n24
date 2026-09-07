91645303dc79a47b7fb510cffa10a09d711a3cae
vibha077 <vibhapatel255@gamil.com>
2026-09-07 17:59:12 +0530

    p update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,20 +39,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script id="meta-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1521013139644650');
-            fbq('track', 'PageView');
-          `}
-        </Script>
+        <script
+          id="meta-pixel"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1521013139644650');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
       </head>
       <body className={`${cormorant.variable} ${manrope.variable}`} suppressHydrationWarning>
         <noscript>
